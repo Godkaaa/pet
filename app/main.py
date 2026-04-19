dab9eaffd203df67d22f76a656b02b1e34866e8e
Godkaaa <egorprosku@gmail.com>
2026-04-19 23:06:02 +0500

    add prometheus

--- app/main.py
+++ app/main.py
@@ -2,9 +2,14 @@ from fastapi import FastAPI
 from redis import Redis
 import os
 import time
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 redis = Redis(host='redis', port=6379, decode_responses=True)
+
+
+#метрики прометеус
+Instrumentator().instrument(app).expose(app)
 
 # Счетчик запросов
 @app.get("/")
